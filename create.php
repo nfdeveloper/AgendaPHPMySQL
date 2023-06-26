7c0d87f443a2323eb9845b698a8c3de96f7f5f6d
nfdeveloper <nfdeveloper04@gmail.com>
2023-06-26 20:24:49 -0300

    Conectando com o Banco de Dados

--- create.php
+++ create.php
@@ -1,8 +1,7 @@
 <?php
     include_once("templates/header.php");
 ?>
-    <h1>Testando Agenda</h1>
-    <i class="fas fa-eye"></i>
+    <h1>Criando Contato</h1>    
 <?php
     include_once("templates/footer.php");
 ?>
---
+++ templates/header.php
@@ -0,0 +1,29 @@
+<?php
+
+    include_once("config/url.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda de Contatos</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css" integrity="sha512-t4GWSVZO1eC8BM339Xd7Uphw5s17a86tIZIj8qRxhnKub6WoyhnrxeCIMeAqBPgdZGlCcG2PrZjMc+Wr78+5Xg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="<?= $BASE_URL ?>index.php">
+                <img src="<?= $BASE_URL ?>img/logo.svg" alt="Agenda">
+            </a>
+            <div>
+                <div class="navbar-nav">
+                    <a href="<?= $BASE_URL ?>index.php" id="home-link active" class="nav-link">Agenda</a>
+                    <a href="<?= $BASE_URL ?>create.php" id="home-link active" class="nav-link">Adicionar Contato</a>
+                </div>
+            </div>
+        </nav>
+    </header>
