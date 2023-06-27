a0ab094a28d711405d7b0d245767bef9ab70f6b3
nfdeveloper <nfdeveloper04@gmail.com>
2023-06-27 01:14:24 -0300

    Deletando contato e finalizando o Projeto

--- config/process.php
+++ config/process.php
@@ -61,6 +61,25 @@
                 echo "Erro: $error";
             }
 
+        }else if($data['type'] === "delete"){
+
+            $id = $data['id'];
+
+            $query = "DELETE FROM contacts WHERE id = :id";
+            
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(":id", $id);
+
+            try{
+
+                $stmt->execute();
+                $_SESSION["msg"] = "Contato excluído com sucesso.";
+        
+            } catch(PDOException $e){
+                // Erro na conexão
+                $error = $e ->getMessage();
+                echo "Erro: $error";
+            }
         }
 
         header("Location:" . $BASE_URL . "../index.php");
